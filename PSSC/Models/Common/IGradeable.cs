d7d73fa18ed27c735760a4f099616ead7afb8353
manufacturist <ioan.g.georgiu@gmail.com>
2016-10-28 09:23:21 +0300

    Moved domains to Domains folder and added empty factories

---
+++ PSSC/Models/Common/IGradeable.cs
@@ -0,0 +1,14 @@
+﻿using Models.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Common
+{
+    public interface IGradeableStudent : IStudent
+    {
+        Credits Credits { get; set; }
+    }
+}
---
+++ PSSC/Models/Common/IStudent.cs
@@ -0,0 +1,15 @@
+﻿using Models.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Common
+{
+    public interface IStudent
+    {
+        RegistrationNumber RegNumber { get; set; }
+        PlainText Name { get; set; }
+    }
+}
