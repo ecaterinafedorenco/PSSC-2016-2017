479d9298da4b4a2ca25b54c32a84b1e651f03af4
ecaterinafedorenco <catea.fedorenco@mail.ru>
2017-01-19 22:42:09 +0200

    chenges 1

--- PSSC/Models/Repository/GradeReportRepository.cs
+++ PSSC/Models/Repository/GradeReportRepository.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using Models.Contexts.Student;
 using System;
 using System.Collections.Generic;
@@ -6,20 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Models.Common.Subject;
-=======
-﻿using System;
-using System.Collections.Generic;
-using Models.Student;
->>>>>>> 78fd45ae9705b12e934894956018ead12d28df4c
 
 namespace Models.Repository
 {
     // Repository for GradeReport aggregat root
-<<<<<<< HEAD
     class GradeReportRepository : IRepository<GradeReport>
-=======
-    class GradeReportRepository : IRepository<Student.GradeReport>
->>>>>>> 78fd45ae9705b12e934894956018ead12d28df4c
     {
         public IEnumerable<GradeReport> List
         {
@@ -44,14 +34,11 @@ namespace Models.Repository
             throw new NotImplementedException();
         }
 
-<<<<<<< HEAD
         public void Save(Subjects aggregate)
         {
             throw new NotImplementedException();
         }
 
-=======
->>>>>>> 78fd45ae9705b12e934894956018ead12d28df4c
         public void Update(GradeReport entity)
         {
             throw new NotImplementedException();
--- PSSC/Models/Repository/StudyYearRepository.cs
+++ PSSC/Models/Repository/StudyYearRepository.cs
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
 ﻿using Models.Contexts.Deanship;
 using System;
 using System.Collections.Generic;
 using Models.Common.Subject;
-=======
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Models.Deanship;
->>>>>>> 78fd45ae9705b12e934894956018ead12d28df4c
 
 namespace Models.Repository
 {
     // Repository for StudyYear aggregat root
-<<<<<<< HEAD
     public class StudyYearRepository : IRepository<StudyYear>
-=======
-    public class StudyYearRepository : IRepository<Deanship.StudyYear>
->>>>>>> 78fd45ae9705b12e934894956018ead12d28df4c
     {
         public IEnumerable<StudyYear> List
         {
@@ -45,14 +31,11 @@ namespace Models.Repository
             throw new NotImplementedException();
         }
 
-<<<<<<< HEAD
         public void Save(Subjects aggregate)
         {
             throw new NotImplementedException();
         }
 
-=======
->>>>>>> 78fd45ae9705b12e934894956018ead12d28df4c
         public void Update(StudyYear entity)
         {
             throw new NotImplementedException();
